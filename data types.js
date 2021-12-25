19ca22c72fb7b2a07d8ae797eb244d5078a05b4e
vinimari <viniciusmarinho44@hotmail.com>
2021-12-25 10:35:01 -0300

    create data types

--- data types.js
+++ data types.js
@@ -2,3 +2,21 @@
   Data Types: 
   undefined, null, boolean, string, symbol, number, and object 
 */ 
+
+// There actually three ways to declare a variable in Javascript 
+
+// 1 - var
+// Changeable. Is going to be able to be used throughout your whole program 
+var myName = "Vini";
+var difference = 45 - 3; 
+
+// 2 - let 
+// Only be used within the scope of where you declared that 
+let ourName = "Vini and Mari";
+
+// 3 - const
+// Never change 
+const pi = 3.14; 
+
+// Variable names and functions names in Javascript are case sensitive. 
+// Is a good practice to use camelCase 
