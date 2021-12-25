c69eeab4fafd48cd574ffa5606c12b7f50e4f55e
vinimari <viniciusmarinho44@hotmail.com>
2021-12-25 10:49:02 -0300

    add file 'math'

---
+++ math.js
@@ -0,0 +1,28 @@
+//Sum 
+var sum = 10 + 10; 
+
+//Subtract 
+var sub = 10 - 5; 
+
+//Multiply 
+var mult = 5 * 5; 
+
+//Divide 
+var div = 10 / 2; 
+
+//remainder 
+// 11 / 3 = 9 | 11 - 9 = 2  
+var remainder = 11 % 3
+
+//Incrementing 
+sum++; 
+ 
+//Decrementing  
+sum--; 
+
+// Augmented Math Operations  
+a += 1; // is the same of:  a = a + 1;
+b -= 1; // is the same of: b = b - 1; 
+c *= 1; // is the same of: c = c * 1;  
+d /= 1; // is the same of: d = d / 1; 
+
