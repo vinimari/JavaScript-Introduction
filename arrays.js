3f595dd46e0970df2c6025c0f026bb5f4136e554
vinimari <viniciusmarinho44@hotmail.com>
2021-12-25 19:14:14 -0300

    add file 'array'

---
+++ arrays.js
@@ -0,0 +1,16 @@
+/* Arrays allow you to sotre several pieces of 
+  data in one place 
+*/  
+var array = ["John", 23] 
+
+//Multidimensional array 
+var multiArray = [["Vini", 21], ["Cecilia", 22]]
+
+//Acess Array data with Indexes 
+console.log(array[0], array[1])
+
+//Acess multidimensional array
+console.log(multiArray[0])    // [Vini, 21]
+console.log(multiArray[0][0]) // [Vini]
+console.log(multiArray[0][1]) // [21]
+console.log(multiArray[1][0]) // [Cecilia]
