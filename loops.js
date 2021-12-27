f7aa8b18c2795facb996a0c8ab22d53af6c36de9
vinimari <viniciusmarinho44@hotmail.com>
2021-12-27 03:53:47 -0300

    for loop added in 'loops

--- loops.js
+++ loops.js
@@ -1,11 +1,24 @@
 // While Loop 
-
 var someArray = []
 
 var count = 0; 
+
+// adding 5 elements
 while(count < 5)
 {
   someArray.push(count)
   count++
 }
 console.log(someArray); 
+
+// For Loop - most commom in JS
+// adding more 5 elements
+// 'for(var cont...)' if the variable was not initializated berfore 
+for(count = 5; count < 10; count++) 
+{
+  someArray.push(count)
+} 
+console.log(someArray)
+
+//obs: count at for loop can be added more than one per cicle
+// can also be subtracted 
